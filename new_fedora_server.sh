34b9565700c13cbd840217e9cb5e00833c2f64cb
matt-andersen <mattandersen0@gmail.com>
2020-01-24 14:09:52 +1000

    Update Docker install command

--- new_fedora_server.sh
+++ new_fedora_server.sh
@@ -4,10 +4,13 @@ echo "Running update"
 sudo yum -y update
 
 echo "Installing other packages"
-sudo yum install -y ansible nano
+sudo yum install -y ansible nano git
 
 
 echo "Installing and configuring Docker"
-sudo yum install -y yum-utils device-mapper-persistent-data lvm2
-sudo yum-config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
-sudo yum install docker-ce
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager \
+    --add-repo \
+    https://download.docker.com/linux/fedora/docker-ce.repo
+sudo dnf install docker-ce docker-ce-cli containerd.io
+sudo systemctl start docker
